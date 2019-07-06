6d2d2338588be660ed0d388b6cb9bd0f2df86948
Yeiij <mayown@gmail.com>
2019-07-06 23:01:56 +0200

    Added custo user model and tests

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,48 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_successfull(self):
+        """
+        Test creating a new user with an email successfull
+        :return:
+        :rtype:
+        """
+        email = 'test@local.host'
+        password = 'testpass123'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        # test
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """
+        Test the email for a new user is normalized
+        :return:
+        :rtype:
+        """
+        email = 'test@LOCAL.HOST'
+        user = get_user_model().objects.create_user(email, 'test123')
+
+        # test
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Test creating  user with no email raise error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test123')
+
+    def test_create_new_superuser(self):
+        """Test creating a new superuser"""
+        user = get_user_model().objects.create_superuser('test@local.host',
+                                                         'test123')
+
+        # test
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
